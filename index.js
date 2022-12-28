c0a45ff4c98379c1d90b3942af8d4a913e48ebd4
James Sgarella <Jimbo8702@gmail.com>
2022-12-28 15:37:52 -0500

    cron job added and express instance

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ const options = {
   dryRun: false,
 };
 
-(async () => {
+const runFollowBot = async () => {
   let browser;
 
   try {
@@ -78,4 +78,6 @@ const options = {
     console.log("Closing browser");
     if (browser) await browser.close();
   }
-})();
+};
+
+module.exports = { runFollowBot };
